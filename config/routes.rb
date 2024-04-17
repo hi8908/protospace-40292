5c72c20205be3c5d96fff4f24ba2705ae903035d
hi8908 <sibutaku774@gmail.com>
2024-04-18 01:23:31 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  root to:'prototypes#index'
+  get 'prototypes/index'
+  get 'messages/index'
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+   resources :comments, only: :create
+  end
+  resources :users, only: :show
+end
